83b242d818cecb65469d218b443f71b21f140086
SvetlanaVerzhbitskaya <svetlana.verzhbitskaya@gmail.com>
2019-09-25 21:49:28 +0300

    Статистика - исправлена функция renderStatistics

--- js/stat.js
+++ js/stat.js
@@ -35,7 +35,7 @@ var writeWelcome = function (ctx, x, y, color) {
   ctx.font = '16px Tahoma';
   ctx.fillText('Ура вы победили!', x, y);
   ctx.fillText('Список результатов:', x, y + FONT_GAP);
-}
+};
 
 var writeText = function (ctx, t, x, y) {
   ctx.fillStyle = TEXT_COLOR;
@@ -54,34 +54,34 @@ var changeOrder = function (arr1, arr2, player) {
       arr2[i] = timeTransfer;
     }
   }
-}
+};
 
 var choseColor = function (ctx, n, player) {
   if (n === player) {
     ctx.fillStyle = 'rgba(255, 0, 0, 1)';
   } else {
     ctx.fillStyle = 'hsl(240, ' + Math.random() * 100 + '%, 50%)';
-    }
-}
+  }
+};
 
 var drawColumn = function (ctx, x, y, height) {
   ctx.fillRect(x, y, BAR_WIDTH, height);
-}
+};
 
 window.renderStatistics = function (ctx, NAMES, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
-  writeWelcome(ctx, CLOUD_X + GAP * 2, CLOUD_Y + GAP * 3, TEXT_COLOR)
+  writeWelcome(ctx, CLOUD_X + GAP * 2, CLOUD_Y + GAP * 3, TEXT_COLOR);
 
   var maxTime = getMaxElement(times);
 
   changeOrder(NAMES, times, 'Вы');
 
   for (var i = 0; i < NAMES.length; i++) {
-    writeText(ctx, Math.round(times[i]), CLOUD_X + BAR_GAP + (BAR_DISTANCE + BAR_WIDTH) * i, CLOUD_Y + TEXT_HEIGHT + FONT_GAP + MAX_HEIGHT - MAX_HEIGHT * times[i] / maxTime - FONT_GAP)
+    writeText(ctx, Math.round(times[i]), CLOUD_X + BAR_GAP + (BAR_DISTANCE + BAR_WIDTH) * i, CLOUD_Y + TEXT_HEIGHT + FONT_GAP + MAX_HEIGHT - MAX_HEIGHT * times[i] / maxTime - FONT_GAP);
     choseColor(ctx, NAMES[i], 'Вы');
-    drawColumn(ctx, CLOUD_X + BAR_GAP + (BAR_DISTANCE + BAR_WIDTH) * i, CLOUD_Y + TEXT_HEIGHT + FONT_GAP + MAX_HEIGHT - MAX_HEIGHT * times[i] / maxTime, MAX_HEIGHT * times[i] / maxTime)
+    drawColumn(ctx, CLOUD_X + BAR_GAP + (BAR_DISTANCE + BAR_WIDTH) * i, CLOUD_Y + TEXT_HEIGHT + FONT_GAP + MAX_HEIGHT - MAX_HEIGHT * times[i] / maxTime, MAX_HEIGHT * times[i] / maxTime);
     writeText(ctx, NAMES[i], CLOUD_X + BAR_GAP + (BAR_DISTANCE + BAR_WIDTH) * i, CLOUD_Y + TEXT_HEIGHT + FONT_GAP + MAX_HEIGHT + GAP);
   }
 };
